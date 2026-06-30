94eb73dc6dd556e3cfd1ee9b3120793cf6ad8407
helpring1004-wq <helpring1004@gmail.com>
2026-06-30 23:45:15 +0900

    fix: force mobile cache refresh

--- app.js
+++ app.js
@@ -208,5 +208,9 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     const registrations = await navigator.serviceWorker.getRegistrations();
     await Promise.all(registrations.map((registration) => registration.unregister()));
+    if ("caches" in window) {
+      const cacheKeys = await caches.keys();
+      await Promise.all(cacheKeys.map((key) => caches.delete(key)));
+    }
   });
 }
