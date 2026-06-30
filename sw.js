9ec6e7a909d07971b86c5c03996bec037a0230f5
helpring1004-wq <helpring1004@gmail.com>
2026-06-30 21:35:32 +0900

    fix: refresh timer display and cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crover-timer-v1';
+const CACHE_NAME = 'crover-timer-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', (event) => {
